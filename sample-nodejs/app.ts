de12e94b73a8365776130a5df898a5004b388d4d
Darrel Pol <poldarrel.dev@gmail.com>
2023-11-04 18:16:22 -0500

    Adding sample post call

--- sample-nodejs/app.ts
+++ sample-nodejs/app.ts
@@ -7,6 +7,19 @@ app.use(bodyParser.json());
 const PORT = 9000;
 
 // endpoints
+/**
+app.post(`/sample-post`, (req, res) => {
+    const {  prop1, prop2, prop3, } = req.body;
+    const invalidPayload = !prop1 || !prop2 || !prop3;
+    if (invalidPayload) {
+        return res.status(400).json({error: "Invalid Payload"})
+    }
+    // do whatever here
+    const result = {};
+    return res.status(201).json({message: "Success!", result});
+})
+**/
+
 app.get('/', (req, res) => {
     res.status(200).send('I am alive, master!');
 });
